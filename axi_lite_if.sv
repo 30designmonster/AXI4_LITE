2defbc4304a41547898fc0984fbf2d8a7b98a299
30designmonster <30praveenin@gmail.com>
2025-06-13 00:03:27 -0400

    Update axi_lite_if.sv
    removing ACLK and ARST

--- axi_lite_if.sv
+++ axi_lite_if.sv
@@ -2,9 +2,6 @@
 //Author:Praveen Saravanan
 interface axi_lite_if #(parameter ADDR_WIDTH = 32, DATA_WIDTH = 32);
 
-  logic                 ACLK;
-  logic                 ARESETn;
-
   // Write Address Channel
   logic [ADDR_WIDTH-1:0] AWADDR;
   logic                  AWVALID;
@@ -32,12 +29,12 @@ interface axi_lite_if #(parameter ADDR_WIDTH = 32, DATA_WIDTH = 32);
   logic                  RREADY;
 
   modport master (
-    input  ARESETn, ACLK, AWREADY, WREADY, BVALID, BRESP, ARREADY, RVALID, RRESP, RDATA,
+    input  AWREADY, WREADY, BVALID, BRESP, ARREADY, RVALID, RRESP, RDATA,
     output AWADDR, AWVALID, WDATA, WVALID, BREADY, ARADDR, ARVALID, RREADY
   );
 
   modport slave (
-    input  ARESETn, ACLK, AWADDR, AWVALID, WDATA, WVALID, BREADY, ARADDR, ARVALID, RREADY,
+    input   AWADDR, AWVALID, WDATA, WVALID, BREADY, ARADDR, ARVALID, RREADY,
     output AWREADY, WREADY, BVALID, BRESP, ARREADY, RVALID, RRESP, RDATA
   );
 
